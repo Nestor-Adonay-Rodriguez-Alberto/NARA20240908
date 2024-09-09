d60f60788d9361849ba43dbed3f8b8191dcc49b1
Nestor.Adonay.Rodriguez.Alberto@gmail.com <nestor.adonay.rodriguez.alberto@gmail.com>
2024-09-08 21:37:59 -0600

    Endpoints Para Iniciar Sesion con Cookie

---
+++ NARA20240908/Controllers/AccountController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace NARA20240908.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+
+        // METODO #1:
+        [HttpPost("login")]
+        public IActionResult Login(string login, string password)
+        {
+
+            // Validar Las Credenciales:
+            if (login == "admin" && password == "12345")
+            {
+                // Lista reclamaciones (claims):
+                var claims = new List<Claim>
+                {
+                    // agrega una reclamacion de nombre con el valor de login:
+                    new Claim(ClaimTypes.Name, login),
+                };
+
+
+                // Crea una identidad de reclamaciones (Claim Identity)
+                // Con el esquema de autentificacion por cookies
+                var claimIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+
+                // Crea propiedades de autorizacion adicionales:
+                var authProperties = new AuthenticationProperties { };
+
+
+                // Inicia sesion del usuario:
+                HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimIdentity), authProperties);
+
+                // Respuesta Exitosa:
+                return Ok("Inicio Sesion Correctamente.");
+            }
+            else
+            {
+                // Respuesta Denegada:
+                return Unauthorized("Credenciales Incorrectas");
+            }
+        }
+
+
+
+        // METODO #2:
+        [HttpPost("logout")]
+        public IActionResult Logout()
+        {
+            // Cierra La Session Del Usuario:
+            HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            // Debuelve la respuesta Exitosa:
+            return Ok("Cerro Cesion Correctamente");
+        }
+
+
+    }
+}
--- NARA20240908/Program.cs
+++ NARA20240908/Program.cs
@@ -1,11 +1,44 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Text.Json;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// AGREGADA: Configuracion Para La Autentificacion De Cookie
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+
+        // Configura el nombre del parametro de URL para redireccionamiento no autorizado
+        options.ReturnUrlParameter = "unauthorized";
+
+        options.Events = new CookieAuthenticationEvents
+        {
+            OnRedirectToLogin = context =>
+            {
+                // Cambia el codigo de estado a no autorizado
+                context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+
+                // Establece el tipo de contenido  como JSON (u otro formato):
+                context.Response.ContentType = "application/json";
+
+                // Objeto:
+                var message = new { error = "No autorizado", message = "Debe iniciar sesion para acceder a este recurso" };
+
+                // Serializa el Objeto en formato JSON 
+                var jsonMessage = JsonSerializer.Serialize(message);
+
+                return context.Response.WriteAsync(jsonMessage);
+            }
+        };
+
+    });
+
 
 var app = builder.Build();
 
@@ -17,9 +50,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication(); // Agregada En este Orden
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
